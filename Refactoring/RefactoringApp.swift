76bdbfe9812709a3d966052c1251f9d7e95616e3
enebin <enebin777@gmail.com>
2025-01-06 19:46:14 +0900

    Initial commit

---
+++ Refactoring/RefactoringApp.swift
@@ -0,0 +1,17 @@
+//
+//  RefactoringApp.swift
+//  Refactoring
+//
+//  Created by Kai Lee on 1/6/25.
+//
+
+import SwiftUI
+
+@main
+struct RefactoringApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RefactoringTests/RefactoringTests.swift
@@ -0,0 +1,17 @@
+//
+//  RefactoringTests.swift
+//  RefactoringTests
+//
+//  Created by Kai Lee on 1/6/25.
+//
+
+import Testing
+@testable import Refactoring
+
+struct RefactoringTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
